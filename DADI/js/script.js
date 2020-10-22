268a02b6c5dd4ebbd4c716cd8aaf76ed031bb1e1
Mattia <mattia.tummolilllo@gmail.com>
2020-10-22 19:05:23 +0200

    Completamento esercizio DADI

--- DADI/js/script.js
+++ DADI/js/script.js
@@ -1,1 +1,25 @@
 //DADI
+var risultati = document.getElementById("risultati");
+var invia = document.getElementById("invia"); //lancia dadi
+invia.addEventListener("click",
+	function() {
+      var nGiocatore = Math.floor(Math.random()*6) +1;;
+      var nPc = Math.floor(Math.random()*6) +1;;
+      var msgVittoria = "Congratulazioni, hai vinto !";
+      var msgSconfitta = "Mi dispiace ma hai perso, ritenta";
+
+      // OUTPUT
+      document.getElementById("nGiocatore").innerHTML = nGiocatore;
+      document.getElementById("nPc").innerHTML = nPc;
+      if (nGiocatore > nPc) {
+         document.getElementById("msgRisultato").innerHTML = msgVittoria;
+      } else {
+         document.getElementById("msgRisultato").innerHTML = msgSconfitta;
+      }
+
+      //DISPLAY RISULTATI ON
+      // document.getElementById("risultati").reset();
+      risultati.className = "dBlock";
+
+	}
+);
