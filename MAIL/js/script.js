89f4caadfc4fde12e8542afbf5e56d85a2a353bf
Mattia <mattia.tummolilllo@gmail.com>
2020-10-22 17:47:02 +0200

    check

--- MAIL/js/script.js
+++ MAIL/js/script.js
@@ -1,4 +1,50 @@
 //MAIL
 
-var prova = ("prova");
-console.log("prova");
+// LISTA EMAIL DEGLI INVITATI
+var listaInvitati = ["topolino@libero.it", "paperino@alice.it", "pippo@gmail.com", "mattia@libero.it"];
+var form = document.getElementById("form");
+
+invia.addEventListener("click",
+	function() {
+      // DICHIARAZIONE VARIABILI INTERNE (INPUT)
+      var emailUtente = document.getElementById("emailUtente");
+      var msgContainer = document.getElementById("msgContainer");
+      var invia = document.getElementById("invia");
+      var check = false;
+
+      // CHECK EMAIL
+      for (var i = 0; i < listaInvitati.length; i++) {
+         listaInvitati[i];
+
+         if (emailUtente === listaInvitati[i]) {
+            check = true;
+         }
+      }
+
+      if (check === true) {
+         console.log("ammesso");
+      } else {
+         console.log("respinto");
+      }
+
+
+
+
+
+
+
+
+
+      // OUTPUT
+
+
+
+
+
+
+      //DISPLAY FORM OFF
+      form.className = "dNone";
+      // DISPLAY MESSAGGIO RISULTATO ON
+      msgContainer.className = "dBlock";
+	}
+);
